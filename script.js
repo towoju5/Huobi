4400b9f6b161d265a40320cc85fc8cddc2e6792b
EMMANUEL TOWOJU <towojuads@gmail.com>
2023-06-16 00:03:02 +0100

    update

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ toggleBtn.addEventListener('click', () => {
   console.log('i am clicked');
   navbarMenu.classList.toggle('show-nav');
 });
+
+$(function(){
+  $('.menu-item').each(function(){
+      $(this).hover(function(){
+          // console.log($(this).siblings('.sub-menu'))
+          // $(this).siblings('.sub-menu').slideToggle(300);            
+          $(this).siblings('.submenu').toggle();
+      });
+  });
+});
+
+let defaultTransform = 0;
+function goNext() {
+    defaultTransform = defaultTransform - 250;
+    var slider = document.getElementById("slider");
+    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
+    slider.style.transform = "translateX(" + defaultTransform + "px)";
+}
+next.addEventListener("click", goNext);
+function goPrev() {
+    var slider = document.getElementById("slider");
+    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+    else defaultTransform = defaultTransform + 250;
+    slider.style.transform = "translateX(" + defaultTransform + "px)";
+}
+setInterval(goNext, 1000)
+prev.addEventListener("click", goPrev);
+
+$('svg').hover(
+  $(this).animate()
+)
