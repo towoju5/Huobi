9890b68446ec3fa7c5d76ab39b5501b32a664661
AdetunjiJakes <68664742+Jamleads@users.noreply.github.com>
2023-06-18 14:08:15 -0400

    mobile menu responsiveness

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 'use strict';
 
 const toggleBtn = document.querySelector('.toggle-btn');
-const navOnSmallScreen = document.querySelector('.navOnSmallScreen');
-console.log(navOnSmallScreen);
+const mobileMenu = document.querySelector('.mobile-menu');
+console.log(mobileMenu);
 console.log(toggleBtn);
 
 toggleBtn.addEventListener('click', () => {
   console.log('i am clicked');
-  navOnSmallScreen.classList.toggle('hidden');
+  mobileMenu.classList.toggle('hidden');
+  mobileMenu.id = 'here';
 });
 
-$(function(){
-  $('.menu-item').each(function(){
-      $(this).hover(function(){            
-          $(this).children('.submenu').stop().slideToggle(300);
-      });
+$(function () {
+  $('.menu-item').each(function () {
+    $(this).hover(function () {
+      $(this).children('.submenu').stop().slideToggle(300);
+    });
   });
 });
 
 let defaultTransform = 0;
 function goNext() {
-    defaultTransform = defaultTransform - 250;
-    var slider = document.getElementById("slider");
-    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
+  defaultTransform = defaultTransform - 250;
+  var slider = document.getElementById('slider');
+  if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7)
+    defaultTransform = 0;
+  slider.style.transform = 'translateX(' + defaultTransform + 'px)';
 }
-next.addEventListener("click", goNext);
+next.addEventListener('click', goNext);
 function goPrev() {
-    var slider = document.getElementById("slider");
-    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
-    else defaultTransform = defaultTransform + 250;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
+  var slider = document.getElementById('slider');
+  if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+  else defaultTransform = defaultTransform + 250;
+  slider.style.transform = 'translateX(' + defaultTransform + 'px)';
 }
-setInterval(goNext, 1000)
-prev.addEventListener("click", goPrev);
+setInterval(goNext, 1000);
+prev.addEventListener('click', goPrev);
